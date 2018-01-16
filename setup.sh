1b44d25ffc08f114bf55be3ce767840fb289cbc6
Michael Rodler <michael.rodler@paluno.uni-due.de>
2018-01-16 13:42:17 +0100

    improved vim checkout

--- setup.sh
+++ setup.sh
@@ -9,7 +9,12 @@ if [[ ! -L ~/.gitconfig ]]; then
     ln -s "$PWD/gitconfig" ~/.gitconfig
 fi
 
-git clone https://github.com/f0rki/dotvim vim
+if [[ ! -d vim ]]; then
+    rm -rf vim || true
+    git clone https://github.com/f0rki/dotvim vim
+else
+    pushd vim; git pull; popd
+fi
 if [[ ! -L ~/.vim ]]; then
     ln -s "$(pwd)/vim" ~/.vim
 fi
