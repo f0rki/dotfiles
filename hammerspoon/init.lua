61f5141a8e673b96dbf612f3d44b309621291594
Michael Rodler <m@mrodler.eu>
2023-06-07 16:38:46 +0200

    hammerspoon hax

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -65,13 +65,24 @@ sky = spoon.SkyRocket:new({
 	resizeMouseButton = "right",
 })
 
+function busyloop(count)
+    local i = 0
+    for j=0,count do
+        i = i + j
+    end
+    return i
+end
+
 -- app launching
 -- alacritty = "/opt/homebrew/alacritty"
 hs.hotkey.bind("alt", "F1", function()
+    -- check if alacritty is running 
 	if hs.application.get("alacritty") == nil then
 		hs.application.launchOrFocus("alacritty")
 	else
+        -- if alacritty is running use the internal mechanism to create a new window
 		hs.execute("/opt/homebrew/bin/alacritty msg create-window")
+        busyloop(1000)
 		hs.application.launchOrFocus("alacritty")
 	end
 end)
@@ -83,3 +94,43 @@ hs.hotkey.bind("alt", "F2", function()
 		hs.application.launchOrFocus("Firefox")
 	end
 end)
+
+function caffeinate_if_docked_at_home() 
+    local am_i_docked = false -- find out if docked at home office
+    if hs.battery.powerSource() == "AC Power" then
+        local external_screen = "C34H89x"
+        for i,screen in ipairs(hs.screen.allScreens()) do
+            if screen:name() == external_screen then
+                am_i_docked = true
+            end
+        end
+    end
+
+    if am_i_docked then
+        -- prevent sleep on systemIdle if docked at home office
+        hs.caffeinate.set("systemIdle", true, false)
+
+        -- disconnect from wifi if we are docked and have ethernet available
+        if hs.wifi.currentNetwork() == "Kaspresknoedl-5" or hs.wifi.currentNetwork() == "Kaspresknoedl-5" then
+            hs.wifi.disassociate()
+        end
+    else
+        -- else allow it
+        hs.caffeinate.set("systemIdle", false, false)
+
+        -- https://github.com/Hammerspoon/hammerspoon/issues/1214
+        -- bit of a hack with applescript...
+        hs.osascript.applescript([[
+tell application "System Events"
+    tell process "SystemUIServer"
+        click (menu bar item 1 of menu bar 1 whose description contains "Wi-Fi")
+        click menu item "Kaspresknoedl-5" of menu 1 of result
+    end tell
+end tell]])
+
+    end
+end
+
+caffeinate_if_docked_at_home()
+local docked_watcher = hs.battery.watcher.new(caffeinate_if_docked_at_home)
+docked_watcher:start()
