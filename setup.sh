937d9f012a8c74699830e36782ee1c947971843d
Michael Rodler <m@mrodler.eu>
2023-10-17 17:33:17 +0200

    updates

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ if command -v nvim >/dev/null 2>&1; then
     pushd ~/.config/nvim
     ./install-plug.sh
     popd
-    nvim --headless -c 'PlugInstall --sync' -c 'q'
-    nvim --headless -c 'UpdateRemotePlugins' -c 'q'
-    nvim --headless -c 'TSInstallSync! rust c cpp python lua bash' -c 'q'
+    nvim --headless -c 'PlugInstall --sync' -c 'qa'
+    nvim --headless -c 'UpdateRemotePlugins' -c 'qa'
+    nvim --headless -c 'TSInstallSync! rust c cpp python lua bash' -c 'qa'
 fi
